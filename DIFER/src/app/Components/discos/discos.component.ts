9c8a3626a6eafe3bd454ecbef87e644c80e1bb03
SamCastro7 <samarah.castro@cetys.edu.mx>
2022-11-21 18:44:32 -0800

    body

---
+++ DIFER/src/app/Components/discos/discos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discos',
+  templateUrl: './discos.component.html',
+  styleUrls: ['./discos.component.css']
+})
+export class DiscosComponent {
+
+}
--- DIFER/src/app/app.module.ts
+++ DIFER/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { prueba } from './prueba/prueba.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { BodyComponent } from './Components/body/body.component';
+import { DiscosComponent } from './Components/discos/discos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    prueba,
-    HelloWorldComponent
+    FooterComponent,
+    HeaderComponent,
+    BodyComponent,
+    DiscosComponent
   ],
   imports: [
     BrowserModule,
